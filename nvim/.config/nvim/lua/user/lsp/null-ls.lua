eb2cb2b79c432a63cdf24116be1f71f15d59e663
RielJ <bulaybulay.rielj@gmail.com>
2022-01-07 18:47:01 +0800

    null-ls config.

--- nvim/.config/nvim/lua/user/lsp/null-ls.lua
+++ nvim/.config/nvim/lua/user/lsp/null-ls.lua
@@ -1,46 +1,60 @@
 local M = {}
 
-M.setup = function ()
-  local null_ls_status_ok, null_ls = pcall(require, "null-ls")
-  if not null_ls_status_ok then
-    return
-  end
+M.setup = function()
+	local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+	if not null_ls_status_ok then
+		return
+	end
 
-  -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-  local formatting = null_ls.builtins.formatting
-  -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-  local diagnostics = null_ls.builtins.diagnostics
-  local code_actions = null_ls.builtins.code_actions
+	-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+	local formatting = null_ls.builtins.formatting
+	-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+	local diagnostics = null_ls.builtins.diagnostics
+	local code_actions = null_ls.builtins.code_actions
+	local hover = null_ls.builtins.hover
 
-  null_ls.setup({
-    debug = false,
-    on_attach = require("user.lsp.handlers").on_attach,
-    debounce = 150,
-    save_after_format = false,
-    sources = {
-      formatting.eslint_d.with({
-       prefer_local = "node_modules/.bin",
-       condition = function(utils)
-          return utils.root_has_file({ ".eslintrc.js" })
-       end
-      }),
-      formatting.eslint_d.with({
-       prefer_local = "node_modules/.bin",
-       condition = function(utils)
-          return not utils.root_has_file({ ".eslintrc.js" })
-       end
-      }),
-      formatting.shfmt,
-      formatting.stylua,
-      formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-      formatting.black.with({ extra_args = { "--fast" } }),
+	null_ls.setup({
+		debug = false,
+		on_attach = require("user.lsp.handlers").on_attach,
+		debounce = 150,
+		save_after_format = false,
+		sources = {
+			-- formatter
+			formatting.prettier.with({
+				prefer_local = "node_modules/.bin",
+				condition = function(utils)
+					return utils.root_has_file({ ".prettierrc" })
+				end,
+			}),
+			formatting.shfmt,
+			formatting.stylua,
+			formatting.black.with({ extra_args = { "--fast" } }),
 
-      diagnostics.eslint_d.with { prefer_local = "node_modules/.bin" },
+			-- diagnostics
+			-- diagnostics.eslint.with({
+			-- 	prefer_local = "node_modules/.bin",
+			-- 	extra_args = { "--no-ignore" },
+			-- 	condition = function(utils)
+			-- 		return utils.root_has_file({ ".eslintrc.json" })
+			-- 	end,
+			-- }),
 
-      code_actions.eslint_d.with { prefer_local = "node_modules/.bin" },
-      -- diagnostics.flake8
-    },
-  })
+			-- code actions
+			code_actions.eslint.with({
+				prefer_local = "node_modules/.bin",
+				extra_args = { "--no-ignore" },
+				condition = function(utils)
+					return utils.root_has_file({ ".eslintrc.json" })
+				end,
+			}),
+			code_actions.gitsigns,
+			code_actions.gitrebase,
+			-- diagnostics.flake8
+
+			-- hover
+			hover.dictionary,
+		},
+	})
 end
 
 return M
