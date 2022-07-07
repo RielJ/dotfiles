718286eaa8a5fdcc68c1c9eb0bb2f6fc8e6a5bc9
RielJ <bulaybulay.rielj@gmail.com>
2022-07-07 11:47:23 +0800

    Sway add emoji shortcut

---
+++ sway/.config/sway/scripts/emoji.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Determine which output is currently active (where the mouse pointer is). 🤔
+MONITOR_ID=$(swaymsg -t get_outputs | jq '[.[].focused] | index(true)')
+
+# Let's pick our emojis! 🎉
+rofimoji --action type --skin-tone moderate --selector-args="-monitor ${MONITOR_ID}"
