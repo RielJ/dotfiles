18fc481aa6a363219b4ea7962b8de02b4f948899
RielJ <bulaybulay.rielj@gmail.com>
2023-05-17 17:04:42 +0800

    NVIM: nvimtree setup refactored

---
+++ nvim/.config/nvim/after/plugin/nvimtree.lua
@@ -0,0 +1,146 @@
+require("nvim-tree").setup({
+  disable_netrw = true,
+  hijack_netrw = true,
+  sort_by = "name",
+  auto_reload_on_write = true,
+  hijack_unnamed_buffer_when_opening = false,
+  hijack_directories = {
+    enable = true,
+    auto_open = true,
+  },
+  open_on_tab = false,
+  hijack_cursor = false,
+  update_cwd = true,
+  diagnostics = {
+    enable = true,
+    show_on_dirs = false,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    },
+  },
+  update_focused_file = {
+    enable = true,
+    update_cwd = true,
+    ignore_list = {},
+  },
+  system_open = {
+    cmd = nil,
+    args = {},
+  },
+  git = {
+    enable = true,
+    ignore = false,
+    timeout = 200,
+  },
+  view = {
+    width = 30,
+    -- height = 30,
+    hide_root_folder = false,
+    side = "left",
+    preserve_window_proportions = false,
+    number = false,
+    relativenumber = false,
+    signcolumn = "yes",
+  },
+  renderer = {
+    add_trailing = false,
+    group_empty = false,
+    highlight_opened_files = "none",
+    indent_markers = {
+      enable = false,
+      icons = {
+        corner = "└ ",
+        edge = "│ ",
+        none = "  ",
+      },
+    },
+    icons = {
+      webdev_colors = true,
+      show = {
+        git = true,
+        folder = true,
+        file = true,
+        folder_arrow = true,
+      },
+      glyphs = {
+        default = "",
+        symlink = "",
+        git = {
+          unstaged = "",
+          staged = "S",
+          unmerged = "",
+          renamed = "➜",
+          deleted = "",
+          untracked = "U",
+          ignored = "◌",
+        },
+        folder = {
+          default = "",
+          open = "",
+          empty = "",
+          empty_open = "",
+          symlink = "",
+        },
+      },
+    },
+    highlight_git = true,
+    root_folder_modifier = ":t",
+  },
+  filters = {
+    dotfiles = true,
+    -- custom = { "node_modules", "\\.cache" },
+    exclude = {},
+  },
+  trash = {
+    cmd = "trash",
+    require_confirm = true,
+  },
+  log = {
+    enable = false,
+    truncate = false,
+    types = {
+      all = false,
+      config = false,
+      copy_paste = false,
+      diagnostics = false,
+      git = false,
+      profile = false,
+    },
+  },
+  actions = {
+    use_system_clipboard = true,
+    change_dir = {
+      enable = true,
+      global = false,
+      restrict_above_cwd = false,
+    },
+    open_file = {
+      quit_on_open = false,
+      resize_window = false,
+      window_picker = {
+        enable = true,
+        chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+        exclude = {
+          filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+          buftype = { "nofile", "terminal", "help" },
+        },
+      },
+    },
+  },
+  on_attach = function(bufnr)
+    local api = require("nvim-tree.api")
+    local keymap = vim.keymap.set
+
+    local function opts(desc)
+      return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+
+    api.config.mappings.default_on_attach(bufnr)
+
+    keymap("n", "v", api.node.open.vertical, opts("CD"))
+    keymap("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
+  end,
+})
