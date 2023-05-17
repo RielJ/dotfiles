cc057895ca165fb63b4426cb6a0cf888b659cc88
RielJ <bulaybulay.rielj@gmail.com>
2023-05-17 17:02:22 +0800

    NVIM: codelens added extra event autocmd

--- nvim/.config/nvim/after/plugin/codelens.lua
+++ nvim/.config/nvim/after/plugin/codelens.lua
@@ -1,6 +1,6 @@
-vim.api.nvim_create_autocmd({ "CursorHold" }, {
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
   pattern = { "*.rs", "*.go", "*.ts", "*.tsx" },
   callback = function()
-    vim.cmd "lua require('rielj.codelens').show_line_sign()"
+    vim.cmd("lua require('rielj.codelens').show_line_sign()")
   end,
 })
