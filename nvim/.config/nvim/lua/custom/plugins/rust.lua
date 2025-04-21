c6ce368d0731ee4b3a767e0f71786419d8853550
RielJ <bulaybulay.rielj@gmail.com>
2025-04-21 19:51:10 +0800

    chore: updates

--- nvim/.config/nvim/lua/custom/plugins/rust.lua
+++ nvim/.config/nvim/lua/custom/plugins/rust.lua
@@ -5,7 +5,12 @@ return {
       require("crates").setup()
     end,
   },
-  {
-    "simrat39/rust-tools.nvim",
-  },
+  -- {
+  --   "simrat39/rust-tools.nvim",
+  -- },
+  -- {
+  --   'mrcjkb/rustaceanvim',
+  --   version = '^5', -- Recommended
+  --   lazy = false,   -- This plugin is already lazy
+  -- }
 }
